d7c56e9ec160dc70ced65c4b53f241569d54699b
Wesley <wei2119@gmail.com>
2024-05-25 16:56:30 +0800

    Remove existing server folder

--- server/models/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  user: require("./user-model"),
-  courseContent: require("./courseContent-model"),
-  courseQuiz: require("./courseQuiz-model"),
-  courseTheme: require("./courseTheme-model"),
-  kautianVocab: require("./kautianVocab-model"),
-  kautianSemantics: require("./kautianSemantics-model"),
-  kautianSentence: require("./kautianSentence-model"),
-};
--- server/routes/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  auth: require("./auth"),
-  course: require("./course-route"),
-  kautian: require("./kautian-route"),
-  vocab: require("./vocab-route"),
-};
