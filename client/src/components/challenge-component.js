9a40030e9cdc27869c6e23662fc8e10e8bd5986f
lyc <lyc125@ntu.im>
2024-05-26 14:51:12 +0800

    final draft

--- client/src/components/challenge-component.js
+++ client/src/components/challenge-component.js
@@ -1,85 +1,75 @@
 import React, { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import CourseService from "../services/course.service";
-import KautianService from "../services/kautian.service";
+import ChallengeService from "../services/challenge.service";
 import "../styles/challenge-style.css";
 
 const ChallengeComponent = ({ currentUser, setCurrentUser }) => {
-  const [flashcardsData, setFlashcardsData] = useState([
-    {
-      front: "點擊空白鍵或是字卡可以翻頁",
-      back: "左右鍵可以用來表示單字的熟悉與否<br/>準備開始囉！",
-    },
-    { front: "第一個單字", back: "First Word" },
-    { front: "第二個單字", back: "Second Word" },
-    { front: "第三個單字", back: "Third Word" },
-    {
-      front: "伊上愛食炕肉。",
-      back: "伊上愛食炕肉。\n I sion̄g ài tsia̍h khoǹg-bah. \n 他最喜歡吃炕肉。 ",
-    },
-    {
-      front: "多謝你！",
-      back: "多謝你！\nTo-siā--lí! \n 謝謝你！ ",
-    },
-    {
-      front: "按呢好！",
-      back: "按呢好！\nÁn-ne hó! \n這樣好！ ",
-    },
-    {
-      front: "我伨你。",
-      back: "我伨你。\nGuá-thīn--lí!\n 我支持你。 ",
-    },
-    {
-      front: "你講啥？",
-      back: "你講啥？\nLí końg siánn?\n 你說什麼？ ",
-    },
-    {
-      front: "著來呢！",
-      back: "著來呢！\nTio̍h lâi--neh! \n 要來喔！ ",
-    },
-    {
-      front: "最後一頁",
-      back: "最後背面",
-    },
-    // 添加更多的單字卡數據
-  ]);
-
-  const navigate = useNavigate();
+  const [flashcardsData, setFlashcardsData] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [position, setPosition] = useState("top"); //top or bottom or
-  const [animationClass, setAnimationClass] = useState(""); //left or right
-
+  const [position, setPosition] = useState("top");
+  const [animationClass, setAnimationClass] = useState("");
   const [lastIndex, setLastIndex] = useState(-1);
-  const [lastPosition, setLastPostion] = useState("bottom");
-
-  const [lastAnimation, setLastAnimation] = useState(""); // 0 means currentIndex is 0;
+  const [lastPosition, setLastPosition] = useState("bottom");
+  const [lastAnimation, setLastAnimation] = useState("");
   const [isAnimating, setIsAnimating] = useState(false);
-
   const [flippedCards, setFlippedCards] = useState("");
-
-  //for logic
-  const [familiar, setFamiliar] = useState([]);
   const [unfamiliar, setUnfamiliar] = useState([]);
-  const [unfamiliarIndex, setUnfamiliarIndex] = useState(0);
+  const [cardsDisplayed, setCardsDisplayed] = useState(0);
+
   const [round, setRound] = useState(0);
   const [upLimit, setUpLimit] = useState(0);
-
+  const [returnDisabled, setReturnDisabled] = useState(true);
   const [buttonsDisabled, setButtonsDisabled] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchUserCollections();
+  }, [currentUser]);
+
+  const fetchUserCollections = async () => {
+    try {
+      const response = await ChallengeService.getUserCollections();
+      const collections = response.data;
+      if (collections) {
+        const flashcards = await Promise.all(
+          collections.map(async ({ ch, no }) => {
+            const response = await ChallengeService.getCardData(ch, no);
+            const { hanji, lomaji, mandarin, audio } = response.data;
+            const noteResponse = await ChallengeService.getNote(ch, no);
+            const note = noteResponse.data;
+            return {
+              front: mandarin,
+              back: `${hanji}\n${lomaji}\n華語：${mandarin}${
+                note ? `\n筆記：${note}` : ""
+              }`,
+              audio,
+            };
+          })
+        );
+        setFlashcardsData(flashcards);
+      }
+    } catch (error) {
+      console.error("Error fetching user collections: ", error);
+    }
+  };
+
+  const handlePlayAudio = (audioUrl, event) => {
+    event.stopPropagation();
+    const audioElement = new Audio(audioUrl);
+    audioElement.play();
+  };
 
   useEffect(() => {
     const handleKeyPress = (event) => {
       if (isAnimating) return;
       if (event.key === " ") {
-        flipCard(currentIndex);
+        flipCard();
+        renderFooter();
       } else if (event.key === "ArrowLeft") {
-        // Handle left arrow key press
         handleLeft();
       } else if (event.key === "ArrowRight") {
-        // Handle right arrow key press
         handleRight();
       } else if (event.key === "ArrowUp") {
-        // Handle up arrow key press
         handleUp();
       }
     };
@@ -101,135 +91,107 @@ const ChallengeComponent = ({ currentUser, setCurrentUser }) => {
     navigate("/login");
   };
 
-  const flipCard = (index) => {
+  const flipCard = () => {
     setFlippedCards(flippedCards === "flipped" ? "" : "flipped");
-
-    if (flippedCards === "flipped") {
-      setButtonsDisabled(true);
-    } else {
-      setButtonsDisabled(false);
-    }
+    setButtonsDisabled(flippedCards === "flipped");
   };
 
-  const cards = document.querySelectorAll(".flashcard");
-
-  const handleLeft = async (event) => {
-    if (
-      isAnimating ||
-      currentIndex >= flashcardsData.length - 1 ||
-      flippedCards == "" ||
-      currentIndex === 0
-    )
-      return;
+  const handleLeft = async () => {
+    if (isAnimating || flippedCards === "") return;
 
     setIsAnimating(true);
-
     setAnimationClass("discard-left");
-    console.log("discard-left here");
 
-    if (round % 3 == 1 && unfamiliarIndex <= unfamiliar.length - 1) {
-      // Insert new unfamiliar card to flashcard
-      setFlashcardsData([
-        ...flashcardsData.slice(0, currentIndex + 1), //insert spot
-        unfamiliar[unfamiliarIndex],
-        ...flashcardsData.slice(currentIndex + 1),
+    let nextIndex = (currentIndex + 1) % flashcardsData.length;
+
+    if (cardsDisplayed >= 3 && unfamiliar.length > 0) {
+      // Show the first unfamiliar card
+      const unfamiliarCard = unfamiliar[0];
+      setFlashcardsData((prevData) => [
+        ...prevData.slice(0, nextIndex),
+        unfamiliarCard,
+        ...prevData.slice(nextIndex),
       ]);
-      setUnfamiliarIndex(unfamiliarIndex + 1);
-      console.log("here is the logic");
+      setUnfamiliar((prevUnfamiliar) => prevUnfamiliar.slice(1)); // Remove the shown unfamiliar card
+      setCardsDisplayed(0); // Reset the counter
+    } else {
+      setCardsDisplayed(cardsDisplayed + 1);
     }
 
-    setUnfamiliar(unfamiliar.concat(flashcardsData[currentIndex]));
-    console.log("unfamiliar is", unfamiliar);
+    setUnfamiliar([...unfamiliar, flashcardsData[currentIndex]]);
     setRound(round + 1);
-    console.log("round is ", round);
 
     await new Promise((resolve) => setTimeout(resolve, 700));
-
     setAnimationClass("");
     setFlippedCards("");
 
-    if (currentIndex > 1) {
-      setUpLimit(currentIndex - 2);
-    }
-    setCurrentIndex(currentIndex + 1);
+    nextIndex = (currentIndex + 1) % flashcardsData.length;
+
+    setCurrentIndex(nextIndex);
     setLastIndex(currentIndex);
-    setLastPostion("buttom");
-
+    setLastPosition("bottom");
     setIsAnimating(false);
-    if (flippedCards == "") {
-      setButtonsDisabled(false);
-    } else {
-      setButtonsDisabled(true);
-    }
-
-    document.body.focus();
+    setReturnDisabled(false);
+    setButtonsDisabled(true);
   };
 
-  const handleRight = async (event) => {
-    if (
-      isAnimating ||
-      currentIndex >= flashcardsData.length - 1 ||
-      flippedCards == ""
-    )
-      return;
+  const handleRight = async () => {
+    if (isAnimating || flippedCards === "") return;
 
     setIsAnimating(true);
     setAnimationClass("discard-right");
 
-    if (round % 3 == 1 && unfamiliarIndex <= unfamiliar.length - 1) {
-      //Insert new unfamiliar card to flashcard
-      setFlashcardsData([
-        ...flashcardsData.slice(0, currentIndex + 1), //insert spot
-        unfamiliar[unfamiliarIndex],
-        ...flashcardsData.slice(currentIndex + 1),
+    let nextIndex = (currentIndex + 1) % flashcardsData.length;
+
+    if (cardsDisplayed >= 3 && unfamiliar.length > 0) {
+      // Show the first unfamiliar card
+      const unfamiliarCard = unfamiliar[0];
+      setFlashcardsData((prevData) => [
+        ...prevData.slice(0, nextIndex),
+        unfamiliarCard,
+        ...prevData.slice(nextIndex),
       ]);
-      setUnfamiliarIndex(unfamiliarIndex + 1);
-      console.log("here is the logic");
+      setUnfamiliar((prevUnfamiliar) => prevUnfamiliar.slice(1)); // Remove the shown unfamiliar card
+      setCardsDisplayed(0); // Reset the counter
+    } else {
+      setCardsDisplayed(cardsDisplayed + 1);
     }
 
     setRound(round + 1);
-    console.log("round is ", round);
 
     await new Promise((resolve) => setTimeout(resolve, 700));
     setAnimationClass("");
     setFlippedCards("");
 
-    if (currentIndex > 1) {
-      setUpLimit(currentIndex - 2);
-    }
-    setCurrentIndex(currentIndex + 1);
-    setLastIndex(currentIndex);
-    setLastPostion("buttom");
+    nextIndex = (currentIndex + 1) % flashcardsData.length;
 
+    setCurrentIndex(nextIndex);
+    setLastIndex(currentIndex);
+    setLastPosition("bottom");
     setIsAnimating(false);
-    if (flippedCards == "") {
-      setButtonsDisabled(false);
-    } else {
-      setButtonsDisabled(true);
-    }
-    document.body.focus();
+    setReturnDisabled(false);
+    setButtonsDisabled(true);
   };
 
   const handleUp = async () => {
-    if (currentIndex <= upLimit || isAnimating) {
-      return;
+    if (currentIndex <= upLimit + 1) {
+      setReturnDisabled(true);
     }
-    setFlippedCards("");
+    if (currentIndex <= upLimit || isAnimating) return;
 
+    setFlippedCards("");
     setLastAnimation("last");
 
     if (currentIndex >= 0) {
       await new Promise((resolve) => setTimeout(resolve, 700));
       setLastAnimation("");
-
-      setPosition("");
-
       setPosition("next");
 
+      const cards = document.querySelectorAll(".flashcard");
       cards[lastIndex].style.transform = "translate(-300px, 0)";
 
       setLastAnimation("lastAnimation");
-      setLastPostion("top");
+      setLastPosition("top");
 
       await new Promise((resolve) => setTimeout(resolve, 700));
       setLastAnimation("");
@@ -237,14 +199,12 @@ const ChallengeComponent = ({ currentUser, setCurrentUser }) => {
       setPosition("top");
 
       cards[lastIndex].style.transform = "translate(0, 0)";
-
-      if (lastIndex >= 0) {
-        setLastIndex(lastIndex - 1);
-        setLastPostion("buttom");
-      }
+      setLastIndex(lastIndex >= 0 ? lastIndex - 1 : -1);
+      setLastPosition("bottom");
     }
 
     setIsAnimating(false);
+    setButtonsDisabled(true);
   };
 
   const renderFlashcard = () => {
@@ -253,29 +213,66 @@ const ChallengeComponent = ({ currentUser, setCurrentUser }) => {
         className={`flashcard 
         ${index === lastIndex ? lastPosition : ""} 
         ${index === lastIndex ? lastAnimation : ""} 
-
         ${index === currentIndex ? position : ""} 
         ${index === currentIndex ? flippedCards : ""} 
         ${index === currentIndex ? animationClass : ""}
-        ${index === currentIndex + 1 ? "next" : ""} 
-        `}
+        ${index === currentIndex + 1 ? "next" : ""}`}
         key={index}
         id={`flashcard-${index}`}
       >
-        <h2 className="reword"></h2>
-        <div className="front" onClick={() => flipCard(index)}>
-          <i className="icon fa-solid fa-play"></i>
+        <div className="front" onClick={flipCard}>
           {data.front}
         </div>
         <div
           className="back"
-          onClick={() => flipCard(index)}
           style={{ whiteSpace: "pre-wrap" }}
+          onClick={flipCard}
         >
-          {data.back}
+          <button
+            id="play-btn"
+            className="play-btn"
+            onClick={(event) => handlePlayAudio(data.audio, event)}
+          >
+            放送
+          </button>
+          <p>{data.back}</p>
         </div>
       </div>
     ));
+  };
+
+  const renderFooter = () => {
+    return (
+      <footer>
+        <button
+          className="footer-button"
+          id="arrow-up"
+          onClick={handleUp}
+          disabled={returnDisabled}
+          style={{ backgroundColor: returnDisabled ? "#777" : "#333" }}
+        >
+          轉頂一條<i className="fa-solid fa-arrow-up"></i>
+        </button>
+        <button
+          className="footer-button"
+          id="arrow-left"
+          disabled={buttonsDisabled}
+          onClick={handleLeft}
+          style={{ backgroundColor: buttonsDisabled ? "#777" : "#333" }}
+        >
+          著閣複習<i className="fa-solid fa-arrow-left"></i>
+        </button>
+        <button
+          className="footer-button"
+          id="arrow-right"
+          disabled={buttonsDisabled}
+          onClick={handleRight}
+          style={{ backgroundColor: buttonsDisabled ? "#777" : "#333" }}
+        >
+          我會曉矣<i className="fa-solid fa-arrow-right"></i>
+        </button>
+      </footer>
+    );
   };
 
   return (
@@ -291,39 +288,44 @@ const ChallengeComponent = ({ currentUser, setCurrentUser }) => {
           </button>
         </div>
       )}
-      {currentUser && (
-        <div>
-          <div className="main">
-            <h2>題目</h2>
-
-            <div className="flashcard-container " id="flashcard-container">
-              {renderFlashcard(currentIndex)}
+      {currentUser && flashcardsData.length > 0 && (
+        <>
+          {flashcardsData.length < 5 ? (
+            <div>
+              <h2>這馬收藏的卡片猶傷少，無法度挑戰喔！</h2>
             </div>
-          </div>
-
-          <footer>
-            <button className="footer-button" id="arrow-up" onClick={handleUp}>
-              轉頂一條<i className="fa-solid fa-arrow-up"></i>
-            </button>
-
-            <button
-              className="footer-button"
-              id="arrow-left"
-              disabled={buttonsDisabled}
-              onClick={handleLeft}
-            >
-              著閣複習<i className="fa-solid fa-arrow-left"></i>
-            </button>
-            <button
-              className="footer-button"
-              id="arrow-right"
-              disabled={buttonsDisabled}
-              onClick={handleRight}
-            >
-              我會曉矣<i className="fa-solid fa-arrow-right"></i>
-            </button>
-          </footer>
-        </div>
+          ) : (
+            <div>
+              <div className="main">
+                <h2>
+                  {flippedCards === "" ? "題目" : "解答"}
+                  <a
+                    className="d-inline-block"
+                    data-bs-toggle="tooltip"
+                    data-bs-placement="top"
+                    title="Space = 反頁，↑ = 轉頂一頁，← = 著閣複習，→ = 我會曉矣。"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-question-circle"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                      <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286m1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94" />
+                    </svg>
+                  </a>
+                </h2>
+                <div className="flashcard-container" id="flashcard-container">
+                  {renderFlashcard()}
+                </div>
+              </div>
+              {renderFooter()}
+            </div>
+          )}
+        </>
       )}
     </div>
   );
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,4 +4,5 @@ module.exports = {
   kautian: require("./kautian-route"),
   vocab: require("./vocab-route"),
   profile: require("./profile-route"),
+  challenge: require("./challenge-route"),
 };
